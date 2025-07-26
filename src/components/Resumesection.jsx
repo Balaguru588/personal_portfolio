53eb85c71e7a7aa074d928642faaafbcd999ec7e
Balaguru <balagurug22ad@psnacet.edu.in>
2025-07-26 14:43:57 +0530

    Update Resumesection.jsx

--- src/components/Resumesection.jsx
+++ src/components/Resumesection.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { FaDownload, FaEye } from "react-icons/fa";
 
 const ResumeSection = () => {
-  const resumePath = "/resume.pdf";
+  const resumePath = "/personal_portfolio/resume.pdf";
 
   return (
     <div className="border-b border-neutral-900 pb-4">
